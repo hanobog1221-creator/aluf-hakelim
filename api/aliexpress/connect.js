ee620c5c1fd694a25307e798433e7e6a26912fec
hanobog1221-creator <hanobog1221@gmail.com>
2026-08-15 17:29:44 +0300

    Add AliExpress OAuth connect endpoint

---
+++ api/aliexpress/connect.js
@@ -0,0 +1,26 @@
+const crypto = require('crypto');
+
+const CALLBACK_URL = 'https://aluf-hakelim-v2-ready.vercel.app/api/aliexpress/callback';
+const APP_KEY = process.env.ALIEXPRESS_APP_KEY || '542860';
+
+module.exports = async function handler(req, res) {
+  if (req.method !== 'GET') {
+    res.setHeader('Allow', 'GET');
+    return res.status(405).send('Method Not Allowed');
+  }
+
+  const state = crypto.randomBytes(24).toString('hex');
+  const secure = process.env.NODE_ENV === 'production' ? '; Secure' : '';
+  res.setHeader('Set-Cookie', `ae_oauth_state=${state}; Path=/; HttpOnly; SameSite=Lax; Max-Age=600${secure}`);
+
+  const params = new URLSearchParams({
+    response_type: 'code',
+    client_id: APP_KEY,
+    redirect_uri: CALLBACK_URL,
+    state,
+    view: 'web',
+    sp: 'ae'
+  });
+
+  return res.redirect(`https://oauth.aliexpress.com/authorize?${params.toString()}`);
+};
