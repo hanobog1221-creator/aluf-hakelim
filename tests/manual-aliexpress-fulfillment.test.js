4bb7f2dd08a39007b0a075b61c16a35c934aa5f6
hanobog1221-creator <hanobog1221@gmail.com>
2026-08-18 01:45:16 +0300

    Enable authenticated admin supplier-payment workflow

--- tests/manual-aliexpress-fulfillment.test.js
+++ tests/manual-aliexpress-fulfillment.test.js
@@ -2,10 +2,12 @@ const test = require('node:test');
 const assert = require('node:assert/strict');
 const { enabled } = require('../api/_lib/manual-aliexpress-fulfillment');
 
-test('manual AliExpress fulfillment is disabled unless explicitly enabled', () => {
+test('manual AliExpress fulfillment is available by default and can be explicitly disabled', () => {
   const previous = process.env.ALIEXPRESS_MANUAL_FULFILLMENT_ENABLED;
   delete process.env.ALIEXPRESS_MANUAL_FULFILLMENT_ENABLED;
   try {
+    assert.equal(enabled(), true);
+    process.env.ALIEXPRESS_MANUAL_FULFILLMENT_ENABLED = 'false';
     assert.equal(enabled(), false);
     process.env.ALIEXPRESS_MANUAL_FULFILLMENT_ENABLED = 'true';
     assert.equal(enabled(), true);
