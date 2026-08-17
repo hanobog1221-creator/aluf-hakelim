cc75e3925c7874bc927d8b6a6b4ddfcc9c7cce28
hanobog1221-creator <hanobog1221@gmail.com>
2026-08-18 00:11:34 +0300

    Remove temporary catalog verification runner

--- api/catalog-sync-b732f4.js
+++
@@ -1,4 +0,0 @@
-const productHandler = require('./aliexpress/product-v2');
-module.exports = async function handler(req, res) {
-  return productHandler({ ...req, method: 'GET', query: { sync: 'all' }, headers: { ...req.headers, authorization: `Bearer ${process.env.CRON_SECRET || ''}` } }, res);
-};
