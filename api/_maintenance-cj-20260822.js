f746916d77c894233b9c9b6b68e937c8b5c9c0a0
hanobog1221-creator <hanobog1221@gmail.com>
2026-08-23 00:08:21 +0300

    Remove imprecise one-time cron jobs

--- api/_maintenance-cj-20260822.js
+++
@@ -1,5 +0,0 @@
-const handler = require('./_maintenance-20260822');
-module.exports = (req, res) => {
-  req.query = { ...(req.query || {}), action: 'cj' };
-  return handler(req, res);
-};
--- api/_maintenance-prepare-20260822.js
+++
@@ -1,5 +0,0 @@
-const handler = require('./_maintenance-20260822');
-module.exports = (req, res) => {
-  req.query = { ...(req.query || {}), action: 'prepare' };
-  return handler(req, res);
-};
